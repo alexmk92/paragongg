19ac04b9931364d77432f25bf3f4e3946cb9a222
Jamie Shepherd <hello@jamie.sh>
2016-05-07 16:00:17 +0100

    CardTooltip basic

---
+++ resources/assets/js/components/CardTooltip.js
@@ -0,0 +1,27 @@
+
+            // <div className="card-tooltip">
+            //     <div className="head-bar">
+            //         <span className="head-card-name">Executioner's Key</span>
+            //         <div className="head-card-row">
+            //             <span className="head-card-type">Equipment</span>
+            //             <span className="head-card-cost">Cost: 2</span>
+            //         </div>
+            //         <div className="head-card-row">
+            //             <span className="head-card-affinity">Corruption</span>
+            //             <span className="head-card-rarity">Common</span>
+            //         </div>
+            //     </div>
+            //     <div class="card-content">
+            //         {cardStats}
+            //         {cardStatsUpgraded}
+            //     </div>
+            // </div>
+
+
+var elements = document.getElementsByClassName('card-tooltip');
+if(elements.length > 0) {
+    // Attach tooltip
+}
+
+// Get all links with attribute / class "card-tooltip-base"
+// Trigger react render on hover
