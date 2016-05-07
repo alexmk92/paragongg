a1acd2bb59abe7a6ed7b4c347557e71d98899edb
Alex Sims <alexander.sims92@gmail.com>
2016-05-07 23:22:56 +0100

    Updated chart to have a point radius of 5

--- resources/assets/js/components/PlayerStatsOverview.js
+++ resources/assets/js/components/PlayerStatsOverview.js
@@ -63,7 +63,7 @@ class PlayerStatsOverview extends Component {
                         pointHoverBackgroundColor: "rgba(75,192,192,1)",
                         pointHoverBorderColor: "rgba(220,220,220,1)",
                         pointHoverBorderWidth: 2,
-                        pointRadius: 20,
+                        pointRadius: 5,
                         pointHitRadius: 20,
                         fill: false,
                     }]
