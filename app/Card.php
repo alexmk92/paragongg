23e53fd4a928637d24bc8aa2944e8920ec04682c
Jamie Shepherd <hello@jamie.sh>
2016-05-09 01:52:51 +0100

    Added fillable to Card model

--- app/Card.php
+++ app/Card.php
@@ -8,6 +8,15 @@ class Card extends Model
 {
     protected $connection = 'mongodb';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'code', 'description', 'type', 'cost', 'upgradeSlots', 'affinity', 'effects',
+    ];
+
     protected $hidden = [
         '_id', 'created_at', 'updated_at'
     ];
