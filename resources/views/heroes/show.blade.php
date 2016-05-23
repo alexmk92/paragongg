fa9f4ae62e71f1381efc71eae1c8dec7d396a46f
Alex Sims <alexander.sims92@gmail.com>
2016-05-23 02:46:35 +0100

    Implemented stats panel for the hero detail page

--- resources/views/heroes/show.blade.php
+++ resources/views/heroes/show.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts/app')
 @section('body')
-    <div id="hero-wrapper">
+    <div id="hero-wrapper" style="background: url('{!! $stageBackground !!}') no-repeat; background-size: contain;">
         <div id="heroes-spinner">SPINNER COMPONENT HERE</div>
-        <div id="hero-stats">STAT COMPONENT HERE</div>
+        <div id="left-wrapper">
+            <div id="hero-stats">STAT COMPONENT HERE</div>
+            <div id="blur-layer"></div>
+        </div>
+        <div id="hero-model-wrapper">
+            <div id="hero-model">
+                <img src="{!! $heroModel !!}" />
+                <div id="blur"></div>
+            </div>
+        </div>
     </div>
     <div id="graph-wrapper">GRAPH COMPONENT HERE</div>
     <div id="abilities">ABILITY COMPONENT HERE</div>
 @endsection
 @section('scripts')
     <script>
+
         var HEROES = {!! json_encode($heroes) !!}
         var HERO = {!! json_encode($hero) !!};
+        console.log(HERO.name);
     </script>
 @endsection
