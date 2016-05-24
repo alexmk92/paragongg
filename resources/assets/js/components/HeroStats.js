7bb98276e26451aac0404b907b1329cc0d5d8aa2
Alex Sims <alexander.sims92@gmail.com>
2016-05-24 16:27:48 +0100

    Added particle layer to /heroes/{hero}

--- resources/assets/js/components/HeroStats.js
+++ resources/assets/js/components/HeroStats.js
@@ -5,47 +5,6 @@ import ReactDOM from 'react-dom';
 import Rcslider from 'rc-slider';
 import { uuid, delimitNumbers } from '../helpers';
 
-document.addEventListener( 'DOMContentLoaded', function () {
-    particlesJS('particle-layer', {
-        particles: {
-            color: '#fff',
-            shape: 'triangle', // "circle", "edge" or "triangle"
-            opacity: 0.2,
-            size: 1,
-            size_random: false,
-            nb: 200,
-            line_linked: {
-                enable_auto: true,
-                distance: 250,
-                color: '#fff',
-                opacity: 0.5,
-                width: 1,
-                condensed_mode: {
-                    enable: true,
-                    rotateX: 600,
-                    rotateY: 600
-                }
-            },
-            anim: {
-                enable: true,
-                speed: 2
-            }
-        },
-        interactivity: {
-            enable: false,
-            mouse: {
-                distance: 250
-            },
-            detect_on: 'canvas', // "canvas" or "window"
-            mode: 'grab'
-        },
-        /* Retina Display Support */
-        retina_detect: false
-    });
-}, false );
-
-
-
 class StatisticPanel extends Component {
     constructor(props) {
         super(props);
