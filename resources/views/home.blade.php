c198fc045e69f231d53c666b357cd3a6f11eab62
Jamie Shepherd <hello@jamie.sh>
2016-05-28 03:06:40 +0100

    End of play homepage redesign /

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,5 +23,101 @@
             </div>
         </a>
     </div>
+    <div class="summary-info-wrapper cf">
+        <div class="summary-info-discussions">
+            <h5>Recent discussion</h5>
+            <ul>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/women/10.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/men/27.jpg" class="user-avatar"/>
+                    <span class="details highlight">Meta</span><span class="details">0 Responses</span>
+                    <span class="content">Is it still worth doing mid, double buff start?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/women/31.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/women/11.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/women/10.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/men/27.jpg" class="user-avatar"/>
+                    <span class="details highlight">Meta</span><span class="details">0 Responses</span>
+                    <span class="content">Is it still worth doing mid, double buff start?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/men/76.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://randomuser.me/api/portraits/women/78.jpg" class="user-avatar"/>
+                    <span class="details highlight">Question</span><span class="details">0 Responses</span>
+                    <span class="content">What is the respawn time for jungle monsters?</span>
+                </a></li>
+            </ul>
+        </div>
+        <div class="summary-info-guides">
+            <h5>Featured guides</h5>
+            <ul>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_ArcBlade/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> jamieshepherd</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 120</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Coil/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> Andhoth</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 92</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Pyro/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> Alexmk92</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 109</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Totem/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> janellenoir</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 83</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+            </ul>
+        </div><div class="summary-info-decks">
+            <h5>Top rated decks</h5>
+            <ul>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_RiftMage/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> Destiny</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 120</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Rampage/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> Lirik</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 164</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Hammer/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> HeatoN</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 127</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+                <li><a href="" class="cf">
+                    <img src="https://s3-eu-west-1.amazonaws.com/paragon.gg/images/heroes/HeroData_Grux/portrait_small.png" class="hero-avatar"/>
+                    <span class="details"><i class="fa fa-user" aria-hidden="true"></i> Pott1</span><span class="details"><i class="fa fa-star" aria-hidden="true"></i> 109</span>
+                    <span class="content">The Ultimate Sparrow Guide - So Good</span>
+                </a></li>
+            </ul>
+        </div>
+    </div>
 
 @endsection
