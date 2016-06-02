fb1dea224430278242a90e8be927a1403a9dde0f
Alex Sims <alexander.sims92@gmail.com>
2016-06-02 19:35:10 +0100

    Added delete options for cards from deck, left and right click both work with left click offering a slide out tooltip and right click offering instant deletion

--- resources/assets/js/components/decks/DeckBuilder.jsx
+++ resources/assets/js/components/decks/DeckBuilder.jsx
@@ -50,19 +50,78 @@ var DeckBuilder = React.createClass({
         });
         return count;
     },
+    buildCount: function() {
+        return this.state.builds.count;
+    },
     toggleModal: function() {
         var modal = this.state.modal ? false : true;
         this.setState({modal: modal});
     },
-    selectCard: function(card) {
+    selectCard: function(card, event) {
+        event.preventDefault();
+
         if(this.state.cardSelected && this.state.cardSelected.code == card.code) {
             this.setState({cardSelected: false, playFlashAnimation: false});
         } else {
             this.setState({cardSelected: card, playFlashAnimation: false});
         }
     },
-    render: function() {
+    deleteCardFromDeck : function(selectedCard, event) {
+        event.preventDefault();
+
+        var newDeck = [];
+        this.state.deck.forEach(function(card) {
+            if(card.code !== selectedCard.code)
+                newDeck.push(card);
+            else if(card.quantity > 1) {
+                card.quantity--;
+                newDeck.push(card);
+            }
+        });
+        this.setState({ deck : newDeck, playFlashAnimation: false });
+    },
+    getCardsInDeck : function() {
         var cardList = this.state.deck.map(function(card) {
+            var className = "";
+            var childClassName = "";
+            if(this.state.lastSelectedCard.code === card.code && this.state.playFlashAnimation) {
+                className += "pulse-card-outer";
+                childClassName += "pulse-card-inner";
+            }
+            if(this.state.cardSelected.code == card.code) {
+                className += " selected";
+            }
+            return (
+                <li className={className}
+                    key={card.code + "_" + Helpers.uuid() }
+                    style={{backgroundImage: 'url(https://s3-eu-west-1.amazonaws.com/paragon.gg/images/cards/'+card.code+'/icon.png)'}}
+                    onContextMenu={this.deleteCardFromDeck.bind(this, card)}
+                    onClick={this.selectCard.bind(this, card)}>
+                    <div className={ "wrapper " + childClassName }>
+                        <span className="count">{card.quantity }x</span>
+                        <span className="name">{card.name}</span>
+                        <span className="cost">{card.cost} CP</span>
+                    </div>
+                    <div className="delete-icon" onClick={this.deleteCardFromDeck.bind(this, card)}>
+                        <i className="fa fa-trash" aria-hidden="true" />
+                    </div>
+                </li>
+            );
+
+        }.bind(this));
+        if(cardList.length === 0) {
+            cardList.push(
+                <li key="no_cards">
+                    <div className="wrapper">
+                        <span>To get started, select a card from the cards menu</span>
+                    </div>
+                </li>
+            );
+        }
+        return cardList;
+    },
+    getBuilds: function() {
+        var buildList = this.state.builds.map(function(build) {
             var className = "";
             var childClassName = "";
             if(this.state.lastSelectedCard.code === card.code && this.state.playFlashAnimation) {
@@ -86,22 +145,31 @@ var DeckBuilder = React.createClass({
             );
 
         }.bind(this));
-        if(cardList.length === 0) {
-            cardList.push(
+        if(buildList.length === 0) {
+            buildList.push(
                 <li key="no_cards">
                     <div className="wrapper">
-                        <span>To get started, select a card from the cards menu</span>
+                        <span>Create your first build by pressing the button below, make sure you have some cards in your deck first!</span>
                     </div>
                 </li>
             );
         }
+        return buildList;
+    },
+    render: function() {
         return (
             <div>
                 <div id="sidebar">
                     <div className="sidebox panel cf">
+                        <h4>Builds <span className="deck-total">{this.buildCount()}/40 Cards</span></h4>
+                        <ul className="deck-list">
+                            {this.getBuilds()}
+                        </ul>
+                    </div>
+                    <div className="sidebox panel cf">
                         <h4>Current Deck <span className="deck-total">{this.deckCount()}/40 Cards</span></h4>
                         <ul className="deck-list">
-                            {cardList}
+                            {this.getCardsInDeck()}
                         </ul>
                     </div>
                 </div>
