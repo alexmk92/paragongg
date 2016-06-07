f7a36dea23009814b95c0fa0adad3f9352abc3db
Jamie Shepherd <hello@jamie.sh>
2016-06-07 01:41:23 +0100

    Tidied up Toptip, added simple documentation, removed old tooltip

--- resources/assets/js/components/libraries/tooltip/Toptip.js
+++ resources/assets/js/components/libraries/tooltip/Toptip.js
@@ -1,37 +1,46 @@
 var Toptip = function() {
     this.active = false;
     this.tooltip = document.createElement("div");
-    // Event listener?
-    this.init = function() {
-        this.tooltip.setAttribute("id", "toptip");
-        var testContent = document.createTextNode("YOLO");
-        this.tooltip.appendChild(testContent);
-        document.body.appendChild(this.tooltip);
-    }
-    this.setContent = function(content) {
-        console.log(content);
-        while (this.tooltip.firstChild) {
-            this.tooltip.removeChild(this.tooltip.firstChild);
-        }
-        this.tooltip.appendChild(content);
-    }
+    this.tooltip.setAttribute("id", "toptip");
+    document.body.appendChild(this.tooltip);
+
+    /**
+     * Show the tooltip, add mousemove listener
+     */
     this.showTooltip = function() {
-        this.active = true;
         this.tooltip.style.display = "block";
-        // Create listener
         document.addEventListener("mousemove", this.updatePosition.bind(this));
     }
+
+    /**
+     * Hide the tooltip, remove mousemove listener
+     */
     this.hideTooltip = function() {
-        this.active = false;
         this.tooltip.style.display = "none";
-        // Destroy listener
         document.removeEventListener("mousemove", this.updatePosition);
     }
+
+    /**
+     * Update the tooltip position
+     * @param event
+     */
     this.updatePosition = function(event) {
         this.tooltip.style.left = event.clientX + "px";
         this.tooltip.style.top = event.clientY  + document.body.scrollTop + "px";
     }
-    this.init();
+
+    /**
+     * Set content of the tooltip
+     * @param content
+     */
+    this.setContent = function(content) {
+        // Delete any existing content
+        while (this.tooltip.firstChild) {
+            this.tooltip.removeChild(this.tooltip.firstChild);
+        }
+        // Append new content
+        this.tooltip.appendChild(content);
+    }
 }
 
 module.exports = Toptip;
