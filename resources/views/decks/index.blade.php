1dd34aca2c993508464dbd0edab2c0b010332dd3
Alex Sims <alexander.sims92@gmail.com>
2016-06-20 00:45:13 +0100

    Deck list items partially styled

--- resources/views/decks/index.blade.php
+++ resources/views/decks/index.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts/app')
 @section('body')
-    <h2>Decks index</h2>
-    <hr/>
-    <a class="btn btn-primary" href="/decks/create">Create a deck</a>
-    <hr/>
-    @foreach($decks as $deck)
-        {{ var_dump($deck) }}
-    @endforeach
+    <div id="deck-list-wrapper">
+    </div>
 @endsection
+<script>
+    var DECKS = [];
+</script>
