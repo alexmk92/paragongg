3c4f3d5b1db04201f463a951a3a446ee3d98eeca
Alex Sims <alexander.sims92@gmail.com>
2016-06-23 14:37:55 +0100

    Implemented spider chart

--- resources/assets/js/components/charts/SpiderWebChart.jsx
+++ resources/assets/js/components/charts/SpiderWebChart.jsx
@@ -5,15 +5,138 @@ require("highcharts/highcharts-more")(Highcharts);
 
 var SpiderWebChart = React.createClass({
     componentDidMount: function() {
-        var options = this.props.options || {
-                
-            };
-        this.renderChart();
+        var theme = this.props.options || {
+            colors: ["#ff6f00", "#64bf22", "#42a9e8", "#aa2ed3", "#2ed3ba", "#e634c2", "#e4e22e"],
+            chart: {
+                backgroundColor: {
+                    color : "rgba(0,0,0,0)"
+                },
+                style: {
+                    fontFamily: "'Unica One', sans-serif"
+                },
+                plotBorderColor: '#5a606f'
+            },
+            title: {
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase',
+                    fontSize: '20px'
+                }
+            },
+            subtitle: {
+                style: {
+                    color: '#E0E0E3',
+                    textTransform: 'uppercase'
+                }
+            },
+            xAxis: {
+                gridLineColor: '#343a4a',
+                labels: {
+                    style: {
+                        fontFamily : "Roboto Condensed",
+                        color: '#fff'
+                    }
+                },
+                lineColor: '#343a4a',
+                minorGridLineColor: '#343a4a',
+                tickColor: '#343a4a',
+                title: {
+                    style: {
+                        color: '#A0A0A3'
+
+                    }
+                }
+            },
+            yAxis: {
+                gridLineColor: '#343a4a',
+                labels: {
+                    style: {
+                        color: '#E0E0E3'
+                    }
+                },
+                lineColor: '#343a4a',
+                minorGridLineColor: '#343a4a',
+                tickColor: '#343a4a',
+                tickWidth: 1,
+                title: {
+                    style: {
+                        color: '#A0A0A3'
+                    }
+                }
+            },
+            tooltip: {
+                backgroundColor: 'rgba(0, 0, 0, 0.85)',
+                style: {
+                    color: '#F0F0F0'
+                }
+            },
+            plotOptions: {
+                series: {
+                    fillOpacity: 0.25,
+                    dataLabels: {
+                        color: '#B0B0B3'
+                    },
+                    marker: {
+                        lineColor: '#333'
+                    }
+                },
+                boxplot: {
+                    fillColor: '#505053'
+                },
+                candlestick: {
+                    lineColor: 'white'
+                },
+                errorbar: {
+                    color: 'white'
+                }
+            },
+            legend: {
+                enabled : false,
+                itemStyle: {
+                    color: '#E0E0E3'
+                },
+                itemHoverStyle: {
+                    color: '#FFF'
+                },
+                itemHiddenStyle: {
+                    color: '#606063'
+                }
+            },
+            credits: {
+                style: {
+                    color: '#666'
+                }
+            },
+            labels: {
+                style: {
+                    color: '#707073'
+                }
+            },
+
+            drilldown: {
+                activeAxisLabelStyle: {
+                    color: '#F0F0F3'
+                },
+                activeDataLabelStyle: {
+                    color: '#F0F0F3'
+                }
+            },
+
+            // special colors for some of the
+            legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+            background2: '#505053',
+            dataLabelsColor: '#B0B0B3',
+            textColor: '#C0C0C0',
+            contrastTextColor: '#F0F0F3',
+            maskColor: 'rgba(255,255,255,0.3)'
+        };
+        this.renderChart(theme);
     },
     componentWillUnmout: function() {
         this.chart.destroy();
     },
-    renderChart: function() {
+    renderChart: function(theme) {
+        Highcharts.setOptions(theme);
         this.chart = new Highcharts.Chart({
             chart: {
                 renderTo : this.props.container || 'spider-web-container',
@@ -48,7 +171,7 @@ var SpiderWebChart = React.createClass({
                 y: 100,
                 layout: 'vertical'
             },
-            series: [{
+            series: this.props.data || [{
                 name: 'Allocated Budget',
                 data: [43000, 19000, 60000, 35000, 17000, 10000],
                 pointPlacement: 'on'
@@ -57,11 +180,12 @@ var SpiderWebChart = React.createClass({
                 data: [50000, 39000, 42000, 31000, 26000, 14000],
                 pointPlacement: 'on'
             }]
-        })
+        });
     },
     render: function() {
+        var styles = { width : this.props.width || "300px", height: this.props.height || "300px" };
         return (
-            <div id={ this.props.container || "spider-web-container" }>
+            <div style={ styles } id={ this.props.container || "spider-web-container" }>
             </div>
         );
     }
