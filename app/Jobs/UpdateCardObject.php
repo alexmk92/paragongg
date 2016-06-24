aacdd56c2a19c8075e482a7d7fcf939766ffc0b4
Jamie Shepherd <hello@jamie.sh>
2016-06-24 01:15:21 +0100

    Added slug to hero object

--- app/Jobs/UpdateCardObject.php
+++ app/Jobs/UpdateCardObject.php
@@ -66,6 +66,7 @@ class UpdateCardObject extends Job implements ShouldQueue
         $cardDetails = json_decode($cardDetails);
 
         $card = Card::where('code', $this->object->id)->first();
+        $card->slug = createSlug($card->name);
         $card->type = $cardDetails->slotType;
         $card->cost = $cardDetails->cost;
         $card->images = $cardDetails->images;
