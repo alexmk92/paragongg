3728eb46bc9fcddaeaa73b418d9ba42f80e65690
Jamie Shepherd <hello@jamie.sh>
2016-06-29 17:20:18 +0100

    Changed example test to /terms as it needs no SQL connections

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
+        $this->visit('/terms')
              ->see('Paragon.gg');
     }
 }
