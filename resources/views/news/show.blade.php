c89b8c40b705e91e309a83e493d718b3c90c86c9
Jamie Shepherd <hello@jamie.sh>
2016-07-05 15:42:36 +0100

    Add avatars to guides + news

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -13,7 +13,7 @@
         <div class="article-body">
             <h1 class="article-title">{{ $article->title }}</h1>
             <div class="article-details">
-                <time>Posted by <strong>{{ $article->author->username }}</strong> on {{ $article->created_at->format('jS F Y, h:i A') }}<span class="updated_at"> (Updated: {{ $article->created_at->format('jS F Y, h:i A') }})</span></time>
+                <time>Posted by <strong><img class="user-avatar inline small" src="{{getAvatar($article->author)}}">{{ $article->author->username }}</strong> on {{ $article->created_at->format('jS F Y, h:i A') }}<span class="updated_at"> (Updated: {{ $article->created_at->format('jS F Y, h:i A') }})</span></time>
             </div>
             <div class="article-body">
                 {!! $articleBody !!}
