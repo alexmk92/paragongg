6b8829ab063df012431e45048a47da44d6224e21
Jamie Shepherd <hello@jamie.sh>
2016-07-06 03:47:26 +0100

    Uncommitted changes

--- resources/views/heroes/show.blade.php
+++ resources/views/heroes/show.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts/app')
 @section('body')
-    <div id="hero-wrapper">
-    </div>
+    <div id="hero-wrapper"></div>
 @endsection
 @section('scripts')
     <script type="text/javascript" src="/build/js/gfycat.min.js"></script>
