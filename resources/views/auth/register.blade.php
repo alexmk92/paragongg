71c409ec67dd3d4fe8d803d6addd6085240fd440
Jamie Shepherd <hello@jamie.sh>
2016-07-17 20:01:11 +0100

    Temporarily removed registration until old users migrated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,20 +6,22 @@
 
         <h2>New account</h2>
 
-        <label>Username</label>
-        <input type="text" name="username" value="{{ old('username') }}">
+        <div class="alert alert-warning">Unfortunately registration is currently disabled while we migrate our old users across to the new site. Please bear with us! We're sorry for the inconvenience.</div>
 
-        <label>E-Mail Address</label>
-        <input type="email" name="email" value="{{ old('email') }}">
+        {{--<label>Username</label>--}}
+        {{--<input type="text" name="username" value="{{ old('username') }}">--}}
 
-        <label>Password</label>
-        <input type="password" class="form-control" name="password">
+        {{--<label>E-Mail Address</label>--}}
+        {{--<input type="email" name="email" value="{{ old('email') }}">--}}
 
-        <label>Confirm Password</label>
-        <input type="password" class="form-control" name="password_confirmation">
+        {{--<label>Password</label>--}}
+        {{--<input type="password" class="form-control" name="password">--}}
 
-        <button type="submit" class="btn">
-            <i class="fa fa-btn fa-user"></i>Register
-        </button>
+        {{--<label>Confirm Password</label>--}}
+        {{--<input type="password" class="form-control" name="password_confirmation">--}}
+
+        {{--<button type="submit" class="btn">--}}
+            {{--<i class="fa fa-btn fa-user"></i>Register--}}
+        {{--</button>--}}
     </form>
 @endsection
