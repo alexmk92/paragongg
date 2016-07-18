8037585f5745b4559616c888426988227437ee78
Jamie Shepherd <hello@jamie.sh>
2016-07-18 16:45:24 +0100

    Gfycat lib added to lib folder in public

--- resources/views/heroes/show.blade.php
+++ resources/views/heroes/show.blade.php
@@ -6,7 +6,7 @@
     <div id="hero-wrapper"></div>
 @endsection
 @section('scripts')
-    <script type="text/javascript" src="/build/js/gfycat.min.js"></script>
+    <script type="text/javascript" src="/js/lib/gfycat.min.js"></script>
     <script type="text/javascript">
         var HERO = {!! json_encode($hero) !!};
     </script>
