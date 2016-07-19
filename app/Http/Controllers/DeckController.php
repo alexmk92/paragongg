fa103c86478706a7424857f5423a44bdd933caaa
Jamie Shepherd <hello@jamie.sh>
2016-07-19 02:02:12 +0100

    Updates to DeckController and API/DeckController for speed

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -35,33 +35,29 @@ class DeckController extends Controller
             ->get();
 
         $decks = [];
-        $decks['featured'] = Deck::where('featured', true);
+        $decks['featured'] = Deck::select('title', 'hero.image', 'hero.name', 'hero.code', 'slug', 'author_id', 'created_at', 'updated_at', 'views', 'votes')->where('featured', true);
         if($hero) $decks['featured'] = $decks['featured']->where('hero.code', $hero->code);
         $decks['featured'] = $decks['featured']->skip($skip)
             ->take($take)
             ->get();
-        $decks['featured'] = $this->sortDecks($decks['featured']);
 
-        $decks['recent'] = Deck::orderBy('updated_at', 'DESC');
+        $decks['recent'] = Deck::select('title', 'hero.image', 'hero.name', 'hero.code', 'slug', 'author_id', 'created_at', 'updated_at', 'views', 'votes')->orderBy('updated_at', 'DESC');
         if($hero) $decks['recent'] = $decks['recent']->where('hero.code', $hero->code);
         $decks['recent'] = $decks['recent']->skip($skip)
             ->take($take)
             ->get();
-        $decks['recent'] = $this->sortDecks($decks['recent']);
 
-        $decks['rated'] = Deck::orderBy('votes', 'DESC');
+        $decks['rated'] = Deck::select('title', 'hero.image', 'hero.name', 'hero.code', 'slug', 'author_id', 'created_at', 'updated_at', 'views', 'votes')->orderBy('votes', 'DESC');
         if($hero) $decks['rated'] = $decks['rated']->where('hero.code', $hero->code);
         $decks['rated'] = $decks['rated']->skip($skip)
             ->take($take)
             ->get();
-        $decks['rated'] = $this->sortDecks($decks['rated']);
 
-        $decks['views'] = Deck::orderBy('views', 'DESC');
+        $decks['views'] = Deck::select('title', 'hero.image', 'hero.name', 'hero.code', 'slug', 'author_id', 'created_at', 'updated_at', 'views', 'votes')->orderBy('views', 'DESC');
         if($hero) $decks['views'] = $decks['views']->where('hero.code', $hero->code);
         $decks['views'] = $decks['views']->skip($skip)
             ->take($take)
             ->get();
-        $decks['views'] = $this->sortDecks($decks['views']);
 
         return view('decks.index')
             ->with('decks', $decks)
