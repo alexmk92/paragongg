cd3c06439b4b1278d19e3ad33bc9e1871b2c7468
Jamie Shepherd <hello@jamie.sh>
2016-07-24 17:51:52 +0100

    Some routes

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['support@paragon.gg' => null, 'Paragon.gg Support' => null],
 
     /*
     |--------------------------------------------------------------------------
