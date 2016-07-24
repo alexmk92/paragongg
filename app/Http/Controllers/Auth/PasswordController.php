0d8576d7b4de770b7ec443ee85ee8e4312b0e560
Jamie Shepherd <hello@jamie.sh>
2016-07-24 18:24:57 +0100

    Updated passwordcontroller with flash emails

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -29,4 +29,58 @@ class PasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * Get the response for after the reset link has been successfully sent.
+     *
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getSendResetLinkEmailSuccessResponse($response)
+    {
+        session()->flash('notification', 'info|A password reset email has been sent to you.');
+
+        return redirect('/login')->with('status', trans($response));
+    }
+
+    /**
+     * Get the response for after the reset link could not be sent.
+     *
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getSendResetLinkEmailFailureResponse($response)
+    {
+        session()->flash('notification', 'warning|Your email was not recognised.');
+
+        return redirect()->back()->withErrors(['email' => trans($response)]);
+    }
+
+    /**
+     * Get the response for after a successful password reset.
+     *
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getResetSuccessResponse($response)
+    {
+        session()->flash('notification', 'success|Your password has been reset. You may now log in.');
+        return redirect('/login')->with('status', trans($response));
+    }
+
+    /**
+     * Get the response for after a failing password reset.
+     *
+     * @param  Request  $request
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getResetFailureResponse(Request $request, $response)
+    {
+        session()->flash('notification', 'warning|Your reset token may have expired.');
+
+        return redirect()->back()
+            ->withInput($request->only('email'))
+            ->withErrors(['email' => trans($response)]);
+    }
 }
