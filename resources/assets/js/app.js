f92a7dc6fa67fa03acbdadc4dc2ecd75ecf74c3c
Alexander Sims <alexander.sims92@gmail.com>
2016-07-26 02:03:57 +0100

    Ajax library now uses Defers

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -27,4 +27,3 @@ require("./components/heroes/HeroesFeed");
 require("./components/heroes/HeroContainer");
 require("./components/heroes/AbilityFeed");
 require("./components/libraries/particles/ParticleWrapper");
-require('./BuildInjector');
