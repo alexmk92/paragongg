cf052d4c46e35eb468b6a433518af0f1172718d6
Jamie Shepherd <hello@jamie.sh>
2016-08-23 01:16:23 +0100

    Fullstop master push

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <p><a href="/">Home</a> | <a href="/news">News</a> | <a href="/guides">Guides</a> | <a href="/decks">Decks</a> | <a href="/cards">Cards</a> | <a href="/heroes">Heroes</a> | <a href="/terms">Terms</a> | <a href="/privacy">Privacy</a></p>
 
-    <p>Paragon.gg is powered by <a href="https://fandom.wikia.com/games">Fandom Games</a>. &copy; 2015-<?php echo date("Y"); ?>.<br/>
+    <p>Paragon.gg is powered by <a href="https://fandom.wikia.com/games">Fandom Games</a> &copy; 2015-<?php echo date("Y"); ?>.<br/>
         Paragon, Unreal, Unreal Engine 4, UE4, and their respective logos are trademarks or registered trademarks of <a href="http://epicgames.com">Epic Games, Inc.</a></p>
 </footer>
